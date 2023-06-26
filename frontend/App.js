7a26843fb39add60e95af429f5a663c7e27f50fc
Kibicho Murage <123640585+kibichomurage@users.noreply.github.com>
2023-06-26 11:50:50 -0400

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './Pages/LandingPage/Landing';
+
 
 const App = () => {
   return(
